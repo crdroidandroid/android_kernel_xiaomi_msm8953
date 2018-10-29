1db7400f7a3a5d458f172353b12e0ecf66c2ecdd
Sultan Alsawaf <sultanxda@gmail.com>
2018-10-29 15:34:21 -0700

    cpu_input_boost: Simplify bitwise operations used to store boost state
    Instead of using a lock, these operations can use atomic operations.
    
    Additionally, use the kernel's BIT() macro and initialize the boost state
    to 0 since the SCREEN_AWAKE bit will be set when the initial screen
    unblank occurs during Android boot anyway.
    
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>
    
    since kernel 3.18 don't have atomic_andnot, Use a compare-exchange loop
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -20,9 +20,9 @@
 #include <linux/slab.h>
 
 /* Available bits for boost_drv state */
-#define SCREEN_AWAKE		(1U << 0)
-#define WAKE_BOOST		(1U << 1)
-#define MAX_BOOST		(1U << 2)
+#define SCREEN_AWAKE		BIT(0)
+#define WAKE_BOOST		BIT(1)
+#define MAX_BOOST		BIT(2)
 
 struct boost_drv {
 	struct workqueue_struct *wq;
@@ -32,35 +32,29 @@ struct boost_drv {
 	struct notifier_block fb_notif;
 	unsigned long max_boost_expires;
 	atomic_t max_boost_dur;
+	atomic_t state;
 	spinlock_t lock;
-	u32 state;
 };
 
 static struct boost_drv *boost_drv_g;
 
 static u32 get_boost_state(struct boost_drv *b)
 {
-	u32 state;
-
-	spin_lock(&b->lock);
-	state = b->state;
-	spin_unlock(&b->lock);
-
-	return state;
+	return atomic_read(&b->state);
 }
 
 static void set_boost_bit(struct boost_drv *b, u32 state)
 {
-	spin_lock(&b->lock);
-	b->state |= state;
-	spin_unlock(&b->lock);
+	atomic_or(state, &b->state);
 }
 
 static void clear_boost_bit(struct boost_drv *b, u32 state)
 {
-	spin_lock(&b->lock);
-	b->state &= ~state;
-	spin_unlock(&b->lock);
+	int old, new;
+	do {
+		old = atomic_read(&b->state);
+		new = old & ~state;
+	} while (atomic_cmpxchg(&b->state, old, new) != old);
 }
 
 static void update_online_cpu_policy(void)
@@ -270,7 +264,7 @@ static int __init cpu_input_boost_init(void)
 	spin_lock_init(&b->lock);
 	INIT_WORK(&b->max_boost, max_boost_worker);
 	INIT_DELAYED_WORK(&b->max_unboost, max_unboost_worker);
-	b->state = SCREEN_AWAKE;
+	atomic_set(&b->state, 0);
 
 	b->cpu_notif.notifier_call = cpu_notifier_cb;
 	ret = cpufreq_register_notifier(&b->cpu_notif, CPUFREQ_POLICY_NOTIFIER);
